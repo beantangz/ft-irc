79badfcd8636e3a3f248827dbc48da85920cd0c5
Valentin Martel <vmartel@f6r4s3.clusters.42paris.fr>
2025-12-18 14:09:40 +0100

    avance des focntions mode

--- Server.hpp
+++ Server.hpp
@@ -1,40 +1,58 @@
 #pragma once
 
 #include "irc.hpp"
+#include "Client.hpp"
+#include "Channel.hpp"
 
 #define MAX_CLIENTS 1024
 
-class Server {
+class Server 
+{
 public:
-	int					 listen_fd;
-	std::vector<Client*>	clients;
-	std::vector<Channel*>   channels;
-	std::string			 password;
+    int                     listen_fd;
+    std::vector<Client*>    clients;
+    std::vector<Channel*>   channels;
+    std::string             password;
 
-	Server(int port, const std::string &password = "");
-	~Server();
+    Server(int port, const std::string &password = "");
+    ~Server();
 
+    // === RUN / POLL ===
+    void    run();
+    void    tchek_listen(int &nfds, struct pollfd *fds);
+    void    tchek_clients(int &nfds, struct pollfd *fds);
+    void    tchek_clients_out(int nfds, struct pollfd *fds);
 
-	void	tchek_listen(int &nfds, struct pollfd *fds);
-	void	tchek_clients(int &nfds, struct pollfd *fds);
-	void tchek_clients_out(int nfds, struct pollfd *fds);
+    // === CLIENT HANDLING ===
+    void    accept_new_client();
+    void    handle_client(Client *c);
+    void    handleBuffer(Client *c, int index, struct pollfd *fds);
+    void    handleCommand(Client *c, std::string &line, int index, struct pollfd *fds);
 
-	void run();
+    void    remove_client(int index, int &nfds, struct pollfd *fds);
 
-	void handleBuffer(Client *c, int index, struct pollfd *fds);
-	void handleCommand(Client *c, std::string& line, int index, struct pollfd *fds);
+    // === FINDERS ===
+    Client*  find_client_by_fd(int fd);
+    Client*  find_client_by_nick(const std::string &nick);
+    Channel* find_channel(const std::string &name);
 
-	Client* find_client_by_fd(int fd);
-	void remove_client(int index, int &nfds, struct pollfd *fds);
+	// MODE fonctions
+    Channel* check_error_mode(Client *c, const std::string &target);
+    void    mode_operator(Client *c, Channel *ch, char sign,
+                          const std::string &param, int index, struct pollfd *fds);
+    void    mode_invite_only(Client *c, Channel *ch, char sign,
+	                         int index, struct pollfd *fds);
+    void    mode_topic_only(Client *c, Channel *ch, char sign,
+                            int index, struct pollfd *fds);
+    void    mode_key(Client *c, Channel *ch, char sign,
+                     const std::string &param, int index, struct pollfd *fds);
+    void    mode_limit(Client *c, Channel *ch, char sign,
+                       const std::string &param, int index, struct pollfd *fds);
+    // === COMMANDS ===
+    void command_MODE(Client *c, std::string target, std::string mode,
+                      std::string param, int index, struct pollfd *fds);
 
-
-	void accept_new_client();
-	void handle_client(Client *c);
-	Channel* find_channel(const std::string &name);
-	Channel* Server::check_error_mode(Client *c, const std::string &target);
-
-	//commmands
-	void command_MODE(Client *c, std::string target, std::string mode, std::string param, int index, struct pollfd *fds);
-	void command_NICK(Client *c, std::string nickname);
-	void command_JOIN(Client *c, std::string channel_name, int index, struct pollfd *fds);
+    void command_NICK(Client *c, std::string nickname);
+    void command_JOIN(Client *c, std::string channel_name,
+                      int index, struct pollfd *fds);
 };
--- command/mode.cpp
+++ command/mode.cpp
@@ -1,6 +1,8 @@
 #include "Server.hpp"
 #include "Channel.hpp"
 
+
+//target = nom du channel
 Channel* Server::check_error_mode(Client *c,
                                   const std::string &target)
 {
@@ -25,13 +27,97 @@ Channel* Server::check_error_mode(Client *c,
 
     return ch;
 }
- 
-							
 
+Client* Server::find_client_by_nick(const std::string &nick)
+{
+    for (size_t i = 0; i < clients.size(); i++)
+    {
+        if (clients[i]->nick == nick)
+            return clients[i];
+    }
+    return NULL;
+}
+
+
+ void Server::mode_operator(Client *c, Channel *ch, char sign, const std::string &param, int index, struct pollfd *fds)
+ {
+	if (param.empty())
+	{
+		send_numeric(c, "ft_irc", 461, "MODE", "Not enough parameters");
+		return;
+	}
+	Client *target_client = find_client_by_nick(param);
+	if (!target_client)
+	{
+		send_numeric(c, "ft_irc", 401, param, "No such Nick");  //a changer
+		return;
+	}
+	if (sign == '+')
+		ch->addOperator(target_client);
+	else
+		ch->removeOperator(target_client);
+	
+    std::string msg = ":" + c->nick + " MODE " + ch->name + " " + sign + "o " + param + "\r\n";
+    // notifier tous les clients
+    for (size_t i = 0; i < ch->clients.size(); i++)
+        ch->clients[i]->queue_send(msg, fds, index);
+ }
+
+ void Server::mode_invite_only(Client *c, Channel *ch, char sign, int index, struct pollfd *fds)
+ {
+	if (sign == '+')
+		ch->invite_only = true;
+	std::string msg = ":" + c->nick + " MODE " + ch->name + " " + sign + "i\r\n";
+    for (size_t i = 0; i < ch->clients.size(); i++)
+        ch->clients[i]->queue_send(msg, fds, index);
+ }
+
+void Server::mode_topic_only(Client *c, Channel *ch, char sign, int index, struct pollfd *fds) 
+{
+    if (sign == '+')
+		ch->topic_op_only = true;
+    std::string msg = ":" + c->nick + " MODE " + ch->name + " " + sign + "t\r\n";
+    for (size_t i = 0; i < ch->clients.size(); i++)
+        ch->clients[i]->queue_send(msg, fds, index);
+}
+
+void Server::mode_key(Client *c, Channel *ch, char sign,
+                      const std::string &param, int index, struct pollfd *fds)
+{
+    if (sign == '+')
+    {
+        if (param.empty())
+        {
+            send_numeric(c, "ft_irc", 461, "MODE", "Not enough parameters");
+            return;
+        }
+        ch->has_key = true;
+        ch->key = param;
+    }
+    else
+    {
+        ch->has_key = false;
+        ch->key.clear();
+    }
+    std::string msg = ":" + c->nick + " MODE " + ch->name + " " + sign + "k\r\n";
+    for (size_t i = 0; i < ch->clients.size(); i++)
+        ch->clients[i]->queue_send(msg, fds, index);
+}
+
+void 
 void Server::command_MODE(Client *c, const std::string target, std::string mode, std::string param, int index, struct pollfd *fds)
 {
    Channel *ch = check_error_mode(c, target);
     if (!ch)
         return;
-	
+	if (mode.length() < 2)
+	{
+		send_numeric(c, "ft_irc", 472, "MODE", "Unknownn mode");
+		return ;
+	}
+	char sign = mode[0];
+	char m = mode[1];
+
+	if (m == 'o')
+		mode_operator(c, ch, sign, param, index, fds);
 }
